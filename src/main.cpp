5cbbfa43ba1ca3839eea4e00d78eb741fb9d9d96
Nhat Minh Le Vo <103168635+NhatMinh0708@users.noreply.github.com>
2025-01-08 22:58:32 +0700

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,34 @@
-#include "../include/class/Authenticator.h"
-#include "../include/controller/UserController.h"
-#include "../include/dao/UserDataHandler.h"
-#include "../include/ui/WelcomeScreen.h" 
-#include <iostream>
+// main.cpp
+#include "RatingSystem.h"
+using namespace std;
 
 int main() {
-    // Hardcoded users for testing
-    UserController userController;
-    WelcomeScreen welcomeScreen;
-    welcomeScreen.displayMenu();
-    
+    RatingSystem rs;
 
-   /*  // Create a test user with hardcoded values
-    std::string username = "hai0901";
-    std::string password = "Hai09012004";
-    std::string fullName = "haingx";
-    std::string phoneNumber = "09012004";
-    std::string email = "tha@example.com";
-    std::string idType = "user";
-    std::string idNumber = "99999";
+    char choice;
+    while (true) {
+        menu();
+        std::cout << "Enter your choice: ";
+        std::cin >> choice;
 
-    User newUser(username, password, fullName, phoneNumber, email, idType, idNumber);
-    
-    // Save the user to file using UserDAO
-    UserDataHandler userDAO;
-    if (userDAO.saveUser(newUser)) {
-        std::cout << "User created successfully.\n";
-    } else {
-        std::cout << "Failed to save user.\n";
+        switch (choice) {
+        case '1':
+            rs.displayAllUsers();
+            break;
+        case '2':
+            rs.addUser();
+            break;
+        case '3':
+            rs.addRating();
+            break;
+        case '4':
+            rs.getAverageRating();
+            break;
+        case '5':
+            cout << "Exiting...\n";
+            return 0;
+        default:
+            cout << "Invalid choice. Please try again.\n";
+        }
     }
-
-    // Now test authentication
-    Authenticator authenticator;
-    std::cout << "\nTesting authentication...\n";
-
-    // Attempt to authenticate with hardcoded credentials
-    if (authenticator.authenticate(username, password)) {
-        std::cout << "Authentication successful. Logged in as " 
-                  << authenticator.getLoggedUser()->getUsername() << "\n";
-    } else {
-        std::cout << "Authentication failed.\n";
-    } */
-
-    return 0;
 }
