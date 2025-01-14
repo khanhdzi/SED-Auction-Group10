6a82e2e1cdb8bfe736844b10c886517c4aac85a5
Nhat Minh Le Vo <103168635+NhatMinh0708@users.noreply.github.com>
2025-01-14 19:49:40 +0700

    Delete src/class/Item.cpp

--- src/class/Item.cpp
+++
@@ -1,8 +0,0 @@
-#include "Item.h"
-
-// Constructor with parameters
-ItemListing::ItemListing(int itemId, const std::string& itemName, const std::string& cat, const std::string& desc, int startBid, int increment, time_t end, int minRating)
-    : id(itemId), name(itemName), category(cat), description(desc), startingBid(startBid), bidIncrement(increment), endTime(end), minBuyerRating(minRating) {}
-
-// Default constructor
-ItemListing::ItemListing() {}
